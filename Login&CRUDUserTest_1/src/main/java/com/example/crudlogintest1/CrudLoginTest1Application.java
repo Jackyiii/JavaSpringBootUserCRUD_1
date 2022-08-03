0e3fce4a3d2d6b33db8723a79795df4f2d123c51
Jackyiii <feiyi0727@gmail.com>
2022-08-03 16:30:38 +0200

    Init CRUD and User

---
+++ Login&CRUDUserTest_1/src/main/java/com/example/crudlogintest1/CrudLoginTest1Application.java
@@ -0,0 +1,13 @@
+package com.example.crudlogintest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudLoginTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudLoginTest1Application.class, args);
+    }
+
+}
