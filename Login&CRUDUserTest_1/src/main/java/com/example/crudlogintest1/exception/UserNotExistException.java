bc8f51f9733b67c5a711fabe88152dde654a1cd6
Jackyiii <feiyi0727@gmail.com>
2022-08-11 15:21:07 +0200

    finish

--- Login&CRUDUserTest_1/src/main/java/com/example/crudlogintest1/exception/UserNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.example.crudlogintest1.exception;
-
-public class UserNotExistException extends RuntimeException{
-    public UserNotExistException(){
-        super("User Not Exit");
-    }
-}
